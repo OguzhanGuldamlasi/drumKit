7bc2a2f6bd5d4e71fcf05965d371408ad915cbbc
OguzhanGuldamlasi <oguldamlasi@etu.edu.tr>
2019-09-27 13:44:19 +0300

    Used eslint

--- drumkit/src/components/DrumKit.js
+++ drumkit/src/components/DrumKit.js
@@ -1,54 +1,60 @@
-import '../styles/DrumKit.css'
-import React from 'react'
+import '../styles/DrumKit.css';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class DrumKit extends React.Component{
-    constructor(props){
-        super(props);
-        this.playSound=this.playSound.bind(this);//We need different methods for each of drumkits.
-        this.endSound=this.endSound.bind(this);
-        this.state={
-            playing:false,
-            className:'simpleButton',
-            module:null
-        };
-        window.addEventListener('keydown',this.playSound);
+class DrumKit extends React.Component {
+  constructor(props) {
+    super(props);
+    this.playSound = this.playSound.bind(this);
+    this.endSound = this.endSound.bind(this);
+    this.state = {
+      playing: false,
+      className: 'simpleButton',
+      module: null,
+    };
+    window.addEventListener('keydown', this.playSound);
 
-        this. audioName=this.props.ring;
-        import(`../sounds/${this.audioName}`+'.wav').then((module)=>
-            this.setState({module:module})
-        );
+    this.audioName = this.props.ring;
+        import(`../sounds/${this.audioName}` + '.wav').then((module) => this.setState({ module }));
+  }
 
+  endSound() {
+    this.setState({
+      playing: false,
+    });
+  }
+
+  playSound(event) {
+    if (event.keyCode === this.props.code || event.type === 'click') {
+      new Audio(this.state.module.default).play();
+    } else {
+      return;
     }
-    endSound(){
-        this.setState({
-            playing:false
-        })
-    }
-    playSound(event){
-        if(event.keyCode===this.props.code||event.type==='click'){
-            new Audio(this.state.module.default).play();
-        }
-        else{
-            return ;
-        }
-        this.setState({
-            playing:true
-        });
-    }
-    render(){
-        const playing=this.state.playing? "isRinging" : "";
-        return (
-            <div>
-                <button
-                    onTransitionEnd={this.endSound}
-                    className={this.state.className}
-                    id={playing}
-                    onClick={this.playSound}>
-                    <kbd>{this.props.letter}</kbd>
-                    <span className='sound'>{this.props.ring}</span>
-                </button>
-            </div>
-        )
-    }
+    this.setState({
+      playing: true,
+    });
+  }
+
+  render() {
+    const playing = this.state.playing ? 'isRinging' : '';
+    return (
+      <div>
+        <button
+          onTransitionEnd={this.endSound}
+          className={this.state.className}
+          id={playing}
+          onClick={this.playSound}
+        >
+          <kbd>{this.props.letter}</kbd>
+          <span className="sound">{this.props.ring}</span>
+        </button>
+      </div>
+    );
+  }
 }
+DrumKit.propTypes = {
+  ring: PropTypes.string,
+  letter: PropTypes.string,
+  code: PropTypes.number,
+};
 export default DrumKit;
--- drumkit/src/components/HomePage.js
+++ drumkit/src/components/HomePage.js
@@ -1,23 +1,25 @@
-import '../styles/HomePage.css'
-import React from 'react'
-import DrumKit from "./DrumKit";
+import '../styles/HomePage.css';
+import React from 'react';
+import DrumKit from './DrumKit';
 
-class HomePage extends React.Component{//Container Component
-    render(){
-        return(
-            <div className='container'>
-                <DrumKit letter='A' ring='clap'  code={65}/>
-                <DrumKit letter='S' ring='hihat'  code={83}/>
-                <DrumKit letter='D' ring='kick'  code={68}/>
-                <DrumKit letter='F' ring='openhat'  code={70}/>
-                <DrumKit letter='G' ring='boom'  code={71}/>
-                <DrumKit letter='H' ring='ride'  code={72}/>
-                <DrumKit letter='J' ring='snare'  code={74}/>
-                <DrumKit letter='K' ring='tom'  code={75}/>
-                <DrumKit letter='L' ring='tink'  code={76}/>
-            </div>
-        )
-    }
+// eslint-disable-next-line react/prefer-stateless-function
+class HomePage extends React.Component {
+  render() {
+    return (
+    // eslint-disable-next-line react/jsx-filename-extension
+      <div className="container">
+        <DrumKit letter="A" ring="clap" code={65} />
+        <DrumKit letter="S" ring="hihat" code={83} />
+        <DrumKit letter="D" ring="kick" code={68} />
+        <DrumKit letter="F" ring="openhat" code={70} />
+        <DrumKit letter="G" ring="boom" code={71} />
+        <DrumKit letter="H" ring="ride" code={72} />
+        <DrumKit letter="J" ring="snare" code={74} />
+        <DrumKit letter="K" ring="tom" code={75} />
+        <DrumKit letter="L" ring="tink" code={76} />
+      </div>
+    );
+  }
 }
 
 export default HomePage;
